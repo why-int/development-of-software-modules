704627341be770570d21fa5719b2ee32e9e33f4c
i.odegov <i.odegov@b-resh.ru>
2026-03-03 11:13:15 +0500

    upd

---
+++ c-sharp/lab-n-three/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using ClosedXML.Excel;
+
+class Program
+{
+    static void Main()
+    {
+        string path = Path.Combine(
+            AppDomain.CurrentDomain.BaseDirectory,
+            "test_file.xlsx"
+        );
+
+        if (!File.Exists(path))
+        {
+            Console.WriteLine("Файл не найден: " + path);
+            return;
+        }
+
+        using (var workbook = new XLWorkbook(path))
+        {
+            foreach (var sheet in workbook.Worksheets)
+            {
+                Console.WriteLine($"\nЛист: {sheet.Name}");
+
+                var range = sheet.RangeUsed();
+
+                foreach (var row in range.Rows())
+                {
+                    foreach (var cell in row.Cells())
+                    {
+                        Console.Write(cell.Value + "\t");
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+
+        Console.WriteLine("\nГотово!");
+    }
+}
